b9a5aa540fb92adb6467f7ff9373ac6df9c3c53c
Coactiv <40586777+Coactiv@users.noreply.github.com>
2023-08-10 16:45:54 -0700

    Add a function to simulate a round of RPS

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,37 @@
 const choice = ["Rock", "Paper", "Scissors"];
+let outcome = "";
 
+// random computer generated choice function
 function getComputerChoice() {
     let computerChoice = choice[(Math.floor(Math.random() * choice.length))];
     return computerChoice;
 }
 console.log(getComputerChoice());
+
+// simulate a game of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        outcome = "It's a Tie!"
+        return outcome;
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        outcome = "You Win! Rock beats Scissors";
+        return outcome;
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        outcome = "You Win! Rock beats Scissors";
+        return outcome;
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        outcome = "You Win! Rock beats Scissors";
+        return outcome;
+    }
+    else {
+        outcome = `You lose! ${computerSelection} beats ${playerSelection}`;
+        return outcome;
+    }
+}
+
+const playerSelection = "rock"; // capitalize first letter from prompt.
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
