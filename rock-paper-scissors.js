b0e5fb98c52ec3722b89a247d7bd7aff134e4497
Coactiv <40586777+Coactiv@users.noreply.github.com>
2023-08-10 19:18:03 -0700

    Add functions to simulate a game & declare winner

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,12 +1,14 @@
 const choice = ["Rock", "Paper", "Scissors"];
 let outcome = "";
+let playerScore = 0;
+let computerScore = 0;
+
 
 // random computer generated choice function
 function getComputerChoice() {
     let computerChoice = choice[(Math.floor(Math.random() * choice.length))];
     return computerChoice;
 }
-console.log(getComputerChoice());
 
 // simulate a game of rock paper scissors
 function playRound(playerSelection, computerSelection) {
@@ -16,22 +18,48 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (playerSelection === "Rock" && computerSelection === "Scissors") {
         outcome = "You Win! Rock beats Scissors";
+        playerScore++;
         return outcome;
     }
     else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        outcome = "You Win! Rock beats Scissors";
+        outcome = "You Win! Paper beats Rock";
+        playerScore++;
         return outcome;
     }
     else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        outcome = "You Win! Rock beats Scissors";
+        outcome = "You Win! Scissors beats Paper";
+        playerScore++;
         return outcome;
     }
     else {
         outcome = `You lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
         return outcome;
     }
 }
 
-const playerSelection = "rock"; // capitalize first letter from prompt.
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (let rounds = 0; rounds < 5; rounds++) {
+
+        let playerSelection = prompt("Choose: Rock, Paper, or Scissors?");
+        playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+        let computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+function checkScore() {
+    if (playerScore > computerScore) {
+        console.log("Player Wins The Match!");
+    }
+    else if (computerScore > playerScore) {
+        console.log("Computer Wins The Match!");
+    }
+    else {
+        console.log("The Match Was A Tie!");
+    }
+}
+
+game();
+checkScore()
